c24aab868394dfbc001751e56ee5d75dcc09f64c
Dan Krieger <dan@codingislife.com>
2021-07-20 07:45:59 -0700

    Add relationships on EventLog

--- src/Vendor/Laravel/Models/EventLog.php
+++ src/Vendor/Laravel/Models/EventLog.php
@@ -15,4 +15,39 @@ class EventLog extends Base
     ];
 
     protected $casts = [ 'extra_data' => 'array' ];
+    
+    protected $_systemModel = false;
+    
+    public function event()
+    {
+        return $this->belongsTo($this->getConfig()->get('event_model'));
+    }
+
+    public function log()
+    {
+        return $this->belongsTo($this->getConfig()->get('log_model'));
+    }
+
+    public function systemClass()
+    {
+        return $this->belongsTo($this->getConfig()->get('system_class_model'), 'class_id');
+    }
+
+    public function getSystemModelAttribute()
+    {
+        if ($this->_systemModel === false) {
+            $systemClass = $this->systemClass;
+
+            if ($systemClass) {
+                $id = $this->class_ref_id;
+                $modelClass = $systemClass->name;
+
+                $this->_systemModel = $modelClass::find($id);
+            } else {
+                $this->_systemModel = null;
+            }
+        }
+
+        return $this->_systemModel;
+    }
 }
